1d29ca808f5fef01bf59aa3ef159b9cc330a5327
Den <plizzz.healme@gmail.com>
2019-12-18 01:18:51 +0300

    tasks05-06 in arrays of arrays

---
+++ src/arraysofarrays/task05/Main.java
@@ -0,0 +1,22 @@
+package arraysofarrays.task05;
+
+import static cleaner.Cleaner.getIntFromUser;
+import static cleaner.Cleaner.print;
+
+public class Main {
+    public static void main(String[] args) {
+        print("Enter matrix half size");
+
+        int halfSize = getIntFromUser();
+        int size = halfSize * 2;
+        int[][] matrix = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i; j++) {
+                matrix[i][j] = i + 1;
+            }
+        }
+
+        print(matrix);
+    }
+}
