6f79de7ecfdbfa751765fd586c822805834ee586
Den <plizzz.healme@gmail.com>
2019-12-21 06:20:23 +0300

    task03 in sort

---
+++ src/sort/task03/Main.java
@@ -0,0 +1,30 @@
+package sort.task03;
+
+import static cleaner.Cleaner.*;
+
+public class Main {
+    public static void main(String[] args) {
+        print("Enter array length");
+        int length = getIntFromUser();
+        int[] array = generateIntegersArray(length);
+        print("Initial array:");
+        print(array);
+
+        for (int i = 0; i < length - 1; i++) {
+            int maxIndex = i;
+
+            for (int j = i + 1; j < length; j++) {
+                if (array[j] > array[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+
+            int temp = array[maxIndex];
+            array[maxIndex] = array[i];
+            array[i] = temp;
+        }
+
+        print("Sorted array:");
+        print(array);
+    }
+}
