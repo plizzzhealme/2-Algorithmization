aeb1202f59d7aebc62086a09b42849ab178742aa
Den <plizzz.healme@gmail.com>
2019-12-29 02:11:50 +0300

    task10 in decomposition

---
+++ src/part04/task10/Main.java
@@ -0,0 +1,35 @@
+package part04.task10;
+
+import static cleaner.Cleaner.getNaturalNumberFromUser;
+import static cleaner.Cleaner.print;
+
+public class Main {
+    public static void main(String[] args) {
+        print("Enter natural number");
+        int n = getNaturalNumberFromUser();
+        print(convertNumberToArray(n));
+    }
+
+    private static int[] convertNumberToArray(int n) {
+        int length = calculateLength(n);
+        int[] number = new int[length];
+        int remainder = n;
+
+        for (int i = length - 1; i >= 0; i--) {
+            number[i] = remainder % 10;
+            remainder /= 10;
+        }
+        return number;
+    }
+
+    private static int calculateLength(int n) {
+        int[] sizeTable = {9, 99, 999, 9999, 99999, 999999, 9999999,
+                99999999, 999999999, Integer.MAX_VALUE};
+
+        for (int i = 0; ; i++) {
+            if (n <= sizeTable[i]) {
+                return i + 1;
+            }
+        }
+    }
+}
