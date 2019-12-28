fe911c31b043c3779a8dee4175a1ea904d670d74
Den <plizzz.healme@gmail.com>
2019-12-28 22:17:55 +0300

    task05 in decomposition

---
+++ src/decomposition/task05/Main.java
@@ -0,0 +1,40 @@
+package decomposition.task05;
+
+import static cleaner.Cleaner.*;
+
+public class Main {
+    public static void main(String[] args) {
+        print("Enter array length");
+
+        int length = getIntFromUser();
+        int[] numbers = generateNaturalNumbersArray(length);
+        print("Numbers:");
+        print(numbers);
+        print("The second largest number is " + findSecondLargestNumber(numbers));
+    }
+
+    /*
+    returns the second largest number in the array,
+    if all numbers are equal returns their value
+     */
+    private static int findSecondLargestNumber(int[] numbers) {
+        int n = numbers.length;
+
+        if (n == 1) {
+            return numbers[0];
+        }
+
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int i : numbers) {
+            if (max < i) {
+                secondMax = max;
+                max = i;
+            } else if (secondMax < i && max != i) {
+                secondMax = i;
+            }
+        }
+        return secondMax;
+    }
+}
