029ddd6b041fe738c009c3dbd630a5a61e56e292
Den <plizzz.healme@gmail.com>
2019-12-28 14:03:29 +0300

    task04 in decomposition

---
+++ src/decomposition/task04/Main.java
@@ -0,0 +1,59 @@
+package decomposition.task04;
+
+import static cleaner.Cleaner.*;
+
+public class Main {
+    public static void main(String[] args) {
+        print("Enter the number of points");
+
+        int n = getNaturalNumberFromUser();
+        int[][] points = generateMatrix(2, n);
+
+        print("Points:");
+        System.out.print("x:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%3d ", points[0][i]);
+        }
+        System.out.println();
+        System.out.print("y:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%3d ", points[1][i]);
+        }
+        System.out.println();
+
+        int[] pair = searchMaxDistancePair(points);
+        int x1 = points[0][pair[0]];
+        int x2 = points[0][pair[1]];
+        int y1 = points[1][pair[0]];
+        int y2 = points[1][pair[1]];
+
+        System.out.printf("The max distance is between points (%d;%d) and (%d;%d)", x1, y1, x2, y2);
+    }
+
+    private static int calculateDistance(int x1, int y1, int x2, int y2) {
+        return (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+    }
+
+    private static int[] searchMaxDistancePair(int[][] points) {
+        int max1 = 0;
+        int max2 = 1;
+        int maxDistance = 0;
+        int n = points[0].length;
+        System.out.println(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int distance = calculateDistance(points[0][i], points[1][i], points[0][j], points[1][j]);
+
+                if (distance > maxDistance) {
+                    maxDistance = distance;
+                    max1 = i;
+                    max2 = j;
+                }
+            }
+        }
+        return new int[]{max1, max2};
+    }
+}
