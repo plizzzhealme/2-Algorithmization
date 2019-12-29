ccbfd44072810573d55c8bffdd4082908038af9f
Den <plizzz.healme@gmail.com>
2019-12-29 04:12:37 +0300

    task12 in decomposition, task 07 in sorts

--- src/part04/task12/Main.java
+++ src/part04/task12/Main.java
@@ -8,5 +8,42 @@ public class Main {
         print("Enter 2 natural numbers");
         int k = getNaturalNumberFromUser();
         int n = getNaturalNumberFromUser();
+        print("Numbers from 1 to " + n + " with one incredible property:");
+        print(buildThisStupidArray(k, n));
+    }
+
+    private static int[] buildThisStupidArray(int k, int n) {
+        int length = countStupidNumbers(k, n);
+        int[] array = new int[length];
+        int index = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (calculateDigitsSum(i) == k) {
+                array[index] = i;
+                index++;
+            }
+        }
+        return array;
+    }
+
+    private static int countStupidNumbers(int k, int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (calculateDigitsSum(i) == k) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static int calculateDigitsSum(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
     }
 }
