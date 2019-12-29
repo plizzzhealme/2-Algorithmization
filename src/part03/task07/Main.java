06620c6dcc2c1536a8b7e1d0887e681e711a0ecb
Den <plizzz.healme@gmail.com>
2019-12-29 03:48:01 +0300

    task12 in decomposition, task 07 in sorts

--- src/part03/task07/Main.java
+++ src/part03/task07/Main.java
@@ -1,7 +1,41 @@
 package part03.task07;
 
+import java.util.Arrays;
+
+import static cleaner.Cleaner.*;
+
 public class Main {
     public static void main(String[] args) {
+        print("Enter 1st sequence length");
 
+        int firstLength = getNaturalNumberFromUser();
+        int[] a = generateIntegersArray(firstLength);
+
+        print("Enter 2nd sequence length");
+
+        int secondLength = getNaturalNumberFromUser();
+        int[] b = generateIntegersArray(secondLength);
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+        print("Sequence A:");
+        print(a);
+        print("Sequence B:");
+        print(b);
+
+        int i = 0;
+        int j = 0;
+        int[] indexes = new int[secondLength];
+
+        while (j < secondLength) {
+            if (i == firstLength || b[j] < a[i]) {
+                indexes[j] = i + j + 1;
+                j++;
+            } else {
+                i++;
+            }
+        }
+        print("Numbers of places to insert sequence B in a new sequence:");
+        print(indexes);
     }
 }
---
+++ src/part04/task12/Main.java
@@ -0,0 +1,12 @@
+package part04.task12;
+
+import static cleaner.Cleaner.getNaturalNumberFromUser;
+import static cleaner.Cleaner.print;
+
+public class Main {
+    public static void main(String[] args) {
+        print("Enter 2 natural numbers");
+        int k = getNaturalNumberFromUser();
+        int n = getNaturalNumberFromUser();
+    }
+}
