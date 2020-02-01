4d1549ce528483d83d29f8ed36efaa5824f79ad4
Den <plizzz.healme@gmail.com>
2020-02-01 20:18:26 +0300

    task16 in decomposition

---
+++ src/part04/task16/Main.java
@@ -0,0 +1,39 @@
+package part04.task16;
+
+import static cleaner.Cleaner.getNaturalNumberFromUser;
+import static cleaner.Cleaner.print;
+
+public class Main {
+    public static void main(String[] args) {
+        print("Enter number's length");
+        int length = getNaturalNumberFromUser();
+        long oddSum = calculateOddSum(length);
+        System.out.println("Odd sum: " + oddSum);
+        System.out.println("Even sum: " + calculateEvenDigitsSum(oddSum));
+    }
+
+    private static long calculateOddSum(int length) {
+        return (long) Math.pow(5, length) * calculateAverageValue(length);
+    }
+
+    private static int calculateAverageValue(int length) {
+        int result = 0;
+
+        for (int i = 0; i < length; i++) {
+            result += 5 * Math.pow(10, i);
+        }
+        return result;
+    }
+
+    private static int calculateEvenDigitsSum(long n) {
+        int sum = 0;
+
+        while (n > 0) {
+            if (n % 2 == 0) {
+                sum += n % 10;
+            }
+            n /= 10;
+        }
+        return sum;
+    }
+}
